b6aee5c34ac281b15e7e9453205f139325d4b004
Shahriyor <70831956+manperfecting@users.noreply.github.com>
2022-08-12 18:29:47 +0500

    feat: implement 7/8 tasks of towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ module.exports = function towelSort (matrix) {
   return  matrix.reduce((abc, deep, i) => {
           deep.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => abc.push(e));
           return abc;}, [])
-  
 }
